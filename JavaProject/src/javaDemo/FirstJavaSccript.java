fdf45095a6f6b1e869fe6846dda6d6c236216301
fazlu <fazlu@192.168.0.14>
2021-12-19 23:00:18 -0500

    First Commit- All Java Files

---
+++ JavaProject/src/javaDemo/FirstJavaSccript.java
@@ -0,0 +1,14 @@
+package javaDemo;
+
+public class FirstJavaSccript {
+	public static void main(String[] args) {
+		
+		int x = 30;
+		String sentence = "I am learning Java";
+		
+		System.out.println(x);
+		System.out.println(sentence);
+		
+	}
+
+}
